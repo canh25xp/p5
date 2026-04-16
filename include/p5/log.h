24997e25c610c4ed893c66eb41f31841f0ece751
Ngô Văn Cảnh <nv.canh@outlook.com>
2026-04-16 13:18:43 +0700

    log: disable verbose log for release build

--- include/p5/log.h
+++ include/p5/log.h
@@ -12,11 +12,12 @@ public:
     static void DisableColoredOutput();
 };
 
-#define PRINT(x) std::cout << "[ PRINT @ " << __func__ << ":" << __LINE__ << " ] " << x << std::endl
-
 #define ERROR(x) std::cerr << Log::ColorRed                                        \
                            << "[ ERROR @ " << __func__ << ":" << __LINE__ << " ] " \
                            << x << Log::ColorNormal << std::endl
+
+#ifdef P5_VERBOSE_LOGGING
+#define PRINT(x) std::cout << "[ PRINT @ " << __func__ << ":" << __LINE__ << " ] " << x << std::endl
 
 #define WARN(x) std::cerr << Log::ColorYellow                                    \
                           << "[ WARN @ " << __func__ << ":" << __LINE__ << " ] " \
@@ -25,3 +26,8 @@ public:
 #define INFO(x) std::cerr << Log::ColorGreen                                     \
                           << "[ INFO @ " << __func__ << ":" << __LINE__ << " ] " \
                           << x << Log::ColorNormal << std::endl
+#else
+#define PRINT(x) ((void)0)
+#define WARN(x) ((void)0)
+#define INFO(x) ((void)0)
+#endif
