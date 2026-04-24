7c8c0734bd9c868f819f8b60ccfff942494cd6c6
Ngô Văn Cảnh <nv.canh@outlook.com>
2026-04-25 00:55:19 +0700

    Add TODO fix

--- include/p5/p5.h
+++ include/p5/p5.h
@@ -20,6 +20,17 @@ class P5 {
     bool Deinitialize();
     bool Reinitialize();
     bool CheckErrors(Error &e, StrBuf &msg);
+
+    // WARN: this call p4 clients with "tag" protocol before any other commands.
+    // Result in every commands afterward force running with protocol tag (Ugly output if the commands not yet defined Output for Result)
+    // TODO: Solutions:
+    // - Reset protocol after done. Not possible since SetProtocol() must happen _before_ the Init() hence cannot be changed afterward.
+    // - Set protocol tags by default and accept breaking compatibility with p4
+    // - Set protocol by default and define output for all p4 commands. Costly but doable.
+    // - Run AutoResolveClient in a separate p5 instance.
+    // - Use a transient ClientApi instance within AutoResolveClient to avoid polluting the main instance.
+    // - Move auto-resolution to a pre-initialization step before m_ClientAPI.Init() is called.
+    // - Implement a smart Result handler that can format tagged output back to plain text for generic commands.
     void AutoResolveClient();
 
     static const int COMMAND_RETRIES = 3; // Specify how many times a command should be retried before the process exits in a failure.
