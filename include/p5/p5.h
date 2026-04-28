1f0f41ca2820a1b9e33a8d2c31881883e63aa46f
Ngô Văn Cảnh <nv.canh@outlook.com>
2026-04-28 22:54:05 +0700

    auto-resolve: move implementation to P5

--- include/p5/p5.h
+++ include/p5/p5.h
@@ -20,6 +20,7 @@ class P5 {
     bool CheckErrors(Error &e, StrBuf &msg);
     ClientUser &Execute(const char *command, std::vector<char *> &argv, ClientUser &user);
     void RefreshIfNeeded();
+    std::string AutoResolve();
 
     static const int COMMAND_RETRIES = 3;             // Specify how many times a command should be retried before the process exits in a failure.
     static const int COMMAND_REFRESH_THRESHOLD = 100; // Specify how many times a connection should be reused before it is refreshed.
