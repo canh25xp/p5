d4110bf7c2b6b905e5107889de69c5311d878e61
Ngô Văn Cảnh <nv.canh@outlook.com>
2026-06-08 10:05:50 +0700

    Remove unused header types.h

--- include/p5/types.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "types/client_spec.h"
-#include "types/depot.h"
-#include "types/filelog.h"
-#include "types/have.h"
-#include "types/spec.h"
-#include "types/view_map.h"
-#include "types/workspace.h"
